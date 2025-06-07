091611dbf36d7314cbaca0c1e47e27d6558a3cc3
slavashilovv@gmail.com <slavashilovv@gmail.com>
2025-06-07 21:50:08 +0300

    Add constants for project

---
+++ constants.py
@@ -0,0 +1,31 @@
+# Персонажи
+GOBLIN_NAME = "Goblin"
+GOBLIN_HEALTH = 30
+GOBLIN_ATTACK = 7
+GOBLIN_DEFENSE = 2
+GOBLIN_EXP_REWARD = 20
+
+TROLL_NAME = "Troll"
+TROLL_HEALTH = 70
+TROLL_ATTACK = 12
+TROLL_DEFENSE = 6
+TROLL_EXP_REWARD = 50
+
+DRAGON_NAME = "Dragon"
+DRAGON_HEALTH = 150
+DRAGON_ATTACK = 20
+DRAGON_DEFENSE = 10
+DRAGON_EXP_REWARD = 150
+
+# Экспо
+EXP_PER_LEVEL = 100
+
+# Список реплик для врагов
+GOBLIN_TAUNTS = ["Я украду твои вещи!", "Еще один глупый искатель приключений!"]
+TROLL_TAUNTS = ["Я раздавлю тебя!", "Ты слишком мал для меня!"]
+DRAGON_TAUNTS = ["Ты осмелился потревожить мой сон?", "Ты не уйдешь отсюда живым!"]
+
+# Предметы
+HEALTH_INCREMENT = 20
+ATTACK_INCREMENT = 5
+DEFENSE_INCREMENT = 3
